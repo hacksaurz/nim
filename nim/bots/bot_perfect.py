0919e64a845f8befd20bcffd02c68f029f466b53
Romain Richard <romain.richard.IT.engineer@gmail.com>
2018-02-11 12:26:49 -0800

    Create the app skeleton

---
+++ nim/bots/bot_perfect.py
@@ -0,0 +1,11 @@
+"""
+A bot that always tries to make the perfect move.
+"""
+from .bot import Bot
+
+
+class BotPerfect(Bot):
+    """
+    A bot that always tries to make the perfect move.
+    """
+    STRAT = "Perfect"
---
+++ nim/bots/bot_random.py
@@ -0,0 +1,11 @@
+"""
+A bot that picks a random number of stones from a random pile.
+"""
+from .bot import Bot
+
+
+class BotRandom(Bot):
+    """
+    A bot that picks a random number of stones from a random pile.
+    """
+    STRAT = "Random"
