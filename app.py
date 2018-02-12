7f7de9426c0c390c598604db40bd6d2109ac8252
Jesse Hix <jessehix@yahoo.com>
2018-02-11 16:15:44 -0800

    Use session, only pass move from UI

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import (
     jsonify,
     render_template,
+    session,
     Flask,
 )
 from json import loads
@@ -9,28 +10,29 @@ from game import Game
 
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 app.game = Game()
 
 
 @app.route('/')
 def index():
-    state = app.game.new_game(
-        min=3,
-        max=12,
-        piles=3,
+    json = loads('{"min": 3, "max": 12, "piles": 3}')
+    session['state'] = app.game.new_game(
+        min=json['min'],
+        max=json['max'],
+        piles=json['piles'],
     )
-    return render_template('index.html', state=state)
+    return render_template(
+        'index.html',
+        state=session['state']
+    )
 
 
 @app.route('/update')
 def update_game_state():
-    # some game state that we get from front end
-    old_state = loads('{"state": [6, 6, 6]}')['state']
-    new_state = {
-        'state': app.game.random.move(old_state)
-    }
-
-    return jsonify(new_state)
+    new_state = session['state']  # minus player move from json
+    session['state'] = app.game.random.move(new_state)
+    return jsonify({'state': session['state']})
 
 
 if __name__ == '__main__':
