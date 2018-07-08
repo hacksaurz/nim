4729e423e64d916c2090e0ebe93dc87b8a3325a6
requires.io <support@requires.io>
2018-07-08 13:57:28 -0700

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'pluggy==0.6.0',
         'py==1.5.4',
         'six==1.11.0',
-        'tox==3.0.0',
+        'tox==3.1.0',
         'virtualenv==16.0.0',
         'Werkzeug==0.14.1',
     ],
