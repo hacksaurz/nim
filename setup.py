a443b8f1def2e4a1658891d89fe75935f53cb201
Romain Richard <romain.richard.IT.engineer@gmail.com>
2018-10-27 15:04:51 -0700

    [requires.io] dependency update on master branch (#63)
    * [requires.io] dependency update
    
    * [requires.io] dependency update
    
    * [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click==7.0",
         "Flask==1.0.2",
-        "itsdangerous==1.0.0",
+        "itsdangerous==1.1.0",
         "Jinja2==2.10",
         "MarkupSafe==1.0",
         "pluggy==0.8.0",
