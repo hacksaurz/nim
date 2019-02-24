371bb9535b93d3cc5e3f3ebae5ebd83feecd4850
requires.io <support@requires.io>
2019-02-23 17:06:13 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "Flask==1.0.2",
         "itsdangerous==1.1.0",
         "Jinja2==2.10",
-        "MarkupSafe==1.1.0",
+        "MarkupSafe==1.1.1",
         "pluggy==0.8.1",
         "py==1.8.0",
         "six==1.12.0",
