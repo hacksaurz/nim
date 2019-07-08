fb704c6e99854694f704c7af1c9804230d1431da
requires.io <support@requires.io>
2019-07-08 14:18:37 -0400

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     classifiers=CLASSIFIERS,
     install_requires=[
         "click==7.0",
-        "Flask==1.1.0",
+        "Flask==1.1.1",
         "itsdangerous==1.1.0",
         "Jinja2==2.10.1",
         "MarkupSafe==1.1.1",
