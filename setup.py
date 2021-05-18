28263187967a40674e7f8541690bf7420d9cdf7e
requires.io <support@requires.io>
2021-05-18 17:52:15 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click==8.0.0",
         "Flask==2.0.0",
-        "itsdangerous==2.0.0",
+        "itsdangerous==2.0.1",
         "Jinja2==3.0.0",
         "MarkupSafe==2.0.0",
         "pluggy==0.13.1",
